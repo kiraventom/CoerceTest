adf40032803f923852be3c478143924642d57ed3
kiraventom <kiraventom@gmail.com>
2024-11-07 21:50:45 +0300

    Init

---
+++ CoerceTest/FooControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CoerceTest;
+
+public partial class FooControl : UserControl
+{
+    public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+        nameof(Text),
+        typeof(string),
+        typeof(FooControl),
+        new FrameworkPropertyMetadata(
+        "0", 
+        FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, 
+        OnTextChanged, OnCoerceText));
+
+    public string Text
+    {
+        get => (string) GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public FooControl()
+    {
+        InitializeComponent();
+        
+        MainTextBox.DataContext = this;
+    }
+
+    private static object OnCoerceText(DependencyObject d, object baseValue)
+    {
+        if (baseValue is string {Length: > 3} s)
+        {
+            baseValue = s[..3];
+        }
+
+        Debug.WriteLine($"CoerceText: {baseValue}");
+        return baseValue;
+    }
+
+    private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        Debug.WriteLine($"TextChanged: {e.NewValue}");
+    }
+}
---
+++ CoerceTest/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace CoerceTest;
+
+public class MainViewModel
+{
+    private string _strValue;
+
+    public string StrValue
+    {
+        get => _strValue;
+        set
+        {
+            Debug.WriteLine($"ViewModel.StrValue_set: {value}");
+            _strValue = value;
+        }
+    }
+}
